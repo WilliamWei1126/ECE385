5c4451b360e974fe2dfb353dab9dad18e3dbc40f
WilliamWei1126 <67813037+WilliamWei1126@users.noreply.github.com>
2026-02-10 21:26:21 -0600

    Lab3 FA4 V1

--- Lab3/Lab3/Lab3.srcs/sources_1/imports/design_source/ripple_adder.sv
+++ Lab3/Lab3/Lab3.srcs/sources_1/imports/design_source/ripple_adder.sv
@@ -12,10 +12,5 @@ module ripple_adder (
 		* Insert code here to implement a ripple adder.
 		* Your code should be completly combinational (don't use always_ff or always_latch).
 		* Feel free to create sub-modules or other files. */
-logic x, y, z;
-logic s,c;
-assign x = 1;
-assign y = 1;
-assign z = 1;
-FA (.x(x),.y(y),.z(z),.s(s),.c(c));
+		
 endmodule
