b724e7098d87b9a138fe6990283e252074a91823
WilliamWei1126 <67813037+WilliamWei1126@users.noreply.github.com>
2026-02-10 23:23:37 -0600

    Lab3 Final V2

--- Lab3/Lab3.runs/impl_1/runme.sh
+++ Lab3/Lab3.runs/impl_1/runme.sh
@@ -41,7 +41,7 @@ EAStep()
 }
 
 # pre-commands:
-/bin/touch .init_design.begin.rst
+/bin/touch .write_bitstream.begin.rst
 EAStep vivado -log adder_toplevel.vdi -applog -m64 -product Vivado -messageDb vivado.pb -mode batch -source adder_toplevel.tcl -notrace
 
 
